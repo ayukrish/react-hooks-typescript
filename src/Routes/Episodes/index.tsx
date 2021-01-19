55dcfc8913002f4c36cd7003f2a46d0abdf41766
Ayush Krishnatray <ayushkrishnatray@197NODMB25134.local>
2021-01-19 11:39:13 +0530

    chore: creating components

--- src/Routes/Episodes/index.tsx
+++ src/Routes/Episodes/index.tsx
@@ -1,4 +1,42 @@
-import React from 'react';
-const Episodes = () => <div>Episodes</div>;
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import Card from '../../components/Card/card';
+import { service } from '../../httpService';
+
+const EpisodeWrapper = styled.section`
+  justify-content: space-around;
+`;
+
+const Episodes = () => {
+  const [episodes, setEpisodes] = useState([]);
+
+  const getData = async (page = 0) => {
+    const data = await service({
+      url: 'https://rickandmortyapi.com/api/episode',
+      method: 'get',
+      page
+    });
+    setEpisodes(data?.results || []);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <EpisodeWrapper className={`flex wrap`}>
+      {episodes.map((item) => (
+        <Card
+          key={item.id}
+          dataObj={{
+            EPISODE: item?.episode,
+            'AIR DATE': item?.air_date
+          }}
+          heading={item?.name}
+        />
+      ))}
+    </EpisodeWrapper>
+  );
+};
 
 export default Episodes;
